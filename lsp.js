d62644705ae407e0516fadad3b6355bda24b7120
lportelli <32228271+lportelli@users.noreply.github.com>
2017-12-09 15:42:44 -0500

    Add linked list to lsp file and logic when a packet is received.

--- lsp.js
+++ lsp.js
@@ -1,10 +1,67 @@
 class LSP {
-    constructor(originID) {
+    constructor(originID, seq) {
         this.origin_router_id = originID;
-        this.sequence = 1;
+        this.sequence = seq;
         this.ttl = 10;
-        this.list = []; // treating the list as an Array for now
+        this.listOfSinglyLists = []; // treating the list as an Array for now list of SinglyLists
     }
 };
 
 global.LSP = LSP;
+
+
+class Node{
+  constructor(id,cost) {
+    this.id = id;
+    this.cost = cost;
+    this.next = null;
+  }
+}
+class SinglyList() {
+  constructor(){
+    this._length = 0;
+    this.head = null;
+  }
+  add(id,cost) {
+    var node = new Node(id,cost),
+        currentNode = this.head;
+
+    // 1st use-case: an empty list
+    if (!currentNode) {
+        this.head = node;
+        this._length++;
+
+        return node;
+    }
+
+    // 2nd use-case: a non-empty list
+    while (currentNode.next) {
+        currentNode = currentNode.next;
+    }
+
+    currentNode.next = node;
+
+    this._length++;
+
+    return node;
+};
+
+searchNodeAt(position) {
+    var currentNode = this.head,
+        length = this._length,
+        count = 1,
+        message = {failure: 'Failure: non-existent node in this list.'};
+
+    // 1st use-case: an invalid position
+    if (length === 0 || position < 1 || position > length) {
+        throw new Error(message.failure);
+    }
+
+    // 2nd use-case: a valid position
+    while (count < position) {
+        currentNode = currentNode.next;
+        count++;
+    }
+
+    return currentNode;
+};
--- router.js
+++ router.js
@@ -4,36 +4,33 @@ class Router {
         this.network = network;
         this.active = true;
         // a dictionary that stores references to other "directly" connected routers
-        this.routing_table = new Map(); // key: router id, value: cost
+        this.routing_table = new Map(); // key: router id, value: {cost:x, tick:y, highest_lsp_seq:z}
         this.network_graph = new Map(); // key: router id (edge), value: an array of all directly connected edges
     }
 
     originatePacket() {
-      if this.active {
-        // TODO:
-        // generate and send an LSP packet to all directly connected routers based on the current state of the network in the        Routing_Table
-
-
-      }
-
     }
 
     receivePacket(packet, id) {
       // only receive the packet if the router is active
       if this.active {
-
-        // Decrement TTL of LSP
-        packet.ttl = packet.ttl -1
+        // Decrement Time To Live of the received LSP
+        packet.ttl--;
         // Send out LSP packet to all directly connected routers (but not the one it was sent from) if
         // (i) TTL > 0
         // and
         // TODO: add to if statement
-        // (ii) no other packet with a higher sequence number was received from    the sending router
-        if (packet.ttl > 0){
+        // (ii) no other packet with a higher sequence number was received from the sending router
+                            // - I'm not sure what that means though
+        if (packet.ttl > 0 && routing_table[id][highest_lsp_seq] < packet.sequence){
+          // add itself to the list of routers on this packet
+          packet.list[id].add(this.id,this.routing_table[id]['cost']);
+          //decrement the tick of the received router id
+          this.routing_table[id]['tick']--;
+          // send the pack to each of the directly connected routers
           for router in routers_table{
             if routers_table[router].id != id{
-              routers_table[router].receivePacket(packet,this.id)
-
+              routers_table[router].receivePacket(packet,this.id);
             }
           }
         }
