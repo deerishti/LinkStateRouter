d6e8b71567467e1344f6615a413e269f5525e671
lportelli <32228271+lportelli@users.noreply.github.com>
2017-12-09 13:01:26 -0500

    add skeletons of functions

--- router.js
+++ router.js
@@ -9,17 +9,36 @@ class Router {
     }
 
     originatePacket() {
-        // TODO:  
+      if this.active {
+        // TODO:
         // generate and send an LSP packet to all directly connected routers based on the current state of the network in the        Routing_Table
+
+
+      }
+
     }
 
     receivePacket(packet, id) {
-        // TODO:
-        // Send out LSP packet to all directly connected routers if 
+      // only receive the packet if the router is active
+      if this.active {
+
+        // Decrement TTL of LSP
+        packet.ttl = packet.ttl -1
+        // Send out LSP packet to all directly connected routers (but not the one it was sent from) if
         // (i) TTL > 0
         // and
+        // TODO: add to if statement
         // (ii) no other packet with a higher sequence number was received from    the sending router
-        
+        if (packet.ttl > 0){
+          for router in routers_table{
+            if routers_table[router].id != id{
+              routers_table[router].receivePacket(packet,this.id)
+
+            }
+          }
+        }
+      }
+
     }
 
 };
